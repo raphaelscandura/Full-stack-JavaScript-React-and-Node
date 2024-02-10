ba8ddd11a33e04a75647cd7996cedd2e1a83f1f3
Raphael Scandura <raphaelscandura@hotmail.com>
2024-02-10 10:10:36 -0300

    styling adjustments

--- scandura-books/src/App.js
+++ scandura-books/src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Logo from './components/Logo';
+import profile from './images/profile.svg';
+import bag from './images/bag.svg';
+
+const navbarItems = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+const navbarIcons = [profile, bag]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo />
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Logo></Logo>
+                <ul className='navbar'>
+                    { navbarItems.map ( (navbarItem) => (
+                        <li className='navbar-item'><p>{navbarItem}</p></li>
+                    ) ) }
+                </ul>
+
+                <ul className='navbar-icons'>
+                    { navbarIcons.map( (icon) => (
+                      <li className="icon"><img src={icon}></img></li>
+                    )) }
+                </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
--- scandura-books/src/components/Logo/index.js
+++ scandura-books/src/components/Logo/index.js
@@ -4,7 +4,10 @@ import './index.css';
 function Logo(){
     return (
         <div className="logo">
-          <img src={logo} alt="Scandura Books" />
+          <img 
+            src={logo} 
+            alt="Scandura Books" 
+          />
           <p> <strong>Scandura</strong> Books</p>
         </div>
     )
